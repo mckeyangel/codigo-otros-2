1bf9e4f1efe6afbbfdce92711fe8448ac9b546b8
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-05-08 23:19:57 -0600

    Se reparan funciones y eliminan duplicidades

--- formulario.js
+++ formulario.js
@@ -1,97 +1,75 @@
-var formulario = document.querySelector("#form")
+var formulario = document.querySelector(".formulario");//Se corrige nombre de la clase
 
-formulario.onsubmit = function(e) {
+formulario.addEventListener("submit",function (e) {//Se corrige evento a addEventListener
 
-  e.prevent();
+  e.preventDefault();//Se agrega Default faltante a la función
   
-  var n = formulario.elements[0]
-  var e = formulario.elements[1]
-  var na = formulario.elements[2]
+  var n = formulario.elements[0];//Se agrega ; a las líneas 8, 9 y 10
+  var e = formulario.elements[1];
+  var na = formulario.elements[2];
 
-  var nombre = n.value
-  var edad = e.value
+  var nombre = n.value;//Se agrega ; líneas 12 y 13
+  var edad = e.value;
 
-  var i = na.selectedIndex
-  var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
+  var i = na.selectedIndex;//Se agrega ; a las siguientes líneas
+  var nacionalidad = na.options[i].value;
+  //Se eliminan console.log
 
   if (nombre.length === 0) {
-    n.classList.add("error")
-  }
+    n.classList.add("error");
+  }//if
   if (edad < 18 || edad > 120) {
-    e.classList.add("error")
-  }
+    e.classList.add("error");
+  }//if
+  if ((nombre.length > 0) && (edad >= 18 && edad < 120) ) {//Se agregan () para mejor lectura y signo = para aceptar la edad de 18
+  agregarInvitado(nombre, edad, nacionalidad);
+  }//if
 
-if (nombre.length > 0 
-  && (edad > 18 
-    && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
-  }
-}
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
-
+});
+//Se elimina botón borrar y solo se deja el botón para eliminar dentro del div de cada invitado
 function agregarInvitado(nombre, edad, nacionalidad) {
 
   if (nacionalidad === "ar") {
-    nacionalidad = "Argentina"
-  }
-  else if (nacionalidad === "mx") {
-    nacionalidad = "Mexicana"
-  }
-  else if (nacionalidad === "vnzl") {
-    nacionalidad = "Venezolana"
-  }
-  else if (nacionalidad === "per") {
-    nacionalidad = "Peruana"
-  }
+    nacionalidad = "Argentina";//Se agrega ; a los if y a los else if
+  } else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  } else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  } else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }//else if
 
-var lista = document.getElementById("lista-de-invitados")
+var lista = document.getElementById("elemento-lista")//Se corrige nombre del Id
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
+var elementoLista = document.createElement("div");
+elementoLista.classList.add("elemento-lista");//Se corrige add
+lista.appendChild(elementoLista);
+//Se eliminan variables que estaban fuera de la función y quedaban duplicadas
 
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
+function crearElemento(descripcion, valor) {//Se agregan ; faltantes dentro de la función
+var spanNombre = document.createElement("span");
+var inputNombre = document.createElement("input");
+var espacio = document.createElement("br");
+spanNombre.textContent = descripcion + ": ";
+inputNombre.value = valor;
+elementoLista.appendChild(spanNombre);
+elementoLista.appendChild(inputNombre);
+elementoLista.appendChild(espacio);
+}//function crearElemento
 
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
+crearElemento("Nombre", nombre);//Se agregan ; faltantes
+crearElemento("Edad", edad);
+crearElemento("Nacionalidad", nacionalidad);
 
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
-
-
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
+var botonBorrar = document.createElement("button");//Se agregan ; faltantes
+botonBorrar.textContent = "Eliminar invitado";
+botonBorrar.id = "boton-borrar";
+var corteLinea = document.createElement("br");
+elementoLista.appendChild(corteLinea);
 elementoLista.appendChild(botonBorrar);
 
- botonBorrar.onclick = function() {
+botonBorrar.onclick = function() {
 // this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
-  }
-}
+botonBorrar.parentNode.remove();
+  }//botonBorrar
+}//funtion agregarInvitado
